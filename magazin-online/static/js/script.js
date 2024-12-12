bd0d8941e9767dae94ee6673723bed3991d2bd02
Druid45ra <168531027+Druid45ra@users.noreply.github.com>
2024-12-12 17:27:08 +0200

    Delete magazin-online directory

--- magazin-online/static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.product button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Produs adăugat în coș!');
-        });
-    });
-});
