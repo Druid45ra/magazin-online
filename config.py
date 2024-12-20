d531a13f41dde8a618edc546a65b5f3db815c3b7
Druid 45ra <168531027+Druid45ra@users.noreply.github.com>
2024-12-20 20:17:07 +0200

    Update config.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'o_cheie_secreta_complexa'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL ') or 'sqlite:///users.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///users.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
